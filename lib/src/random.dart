6618716d815310fb5b2de8016de6ba519f112cc0
Jesper Håkansson <drager@programmers.se>
2015-05-30 15:54:20 +0200

    doc: Doc comments to the random methods
    [ci skip]

--- lib/src/random.dart
+++ lib/src/random.dart
@@ -2,10 +2,15 @@ import 'dart:math';
 
 var _rng = new Random();
 
-randomElement(List list) {
+/// Plucks a random element from the given [list].
+String randomElement(List list) {
   return list[_rng.nextInt(list.length)];
 }
 
+/// Generates a random number from the [max] value
+/// and by the number of [times].
+///
+/// Returns a list of numbers.
 List<int> randomNumber(num max, num times) {
   var number = [];
 
