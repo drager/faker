732ccce50c11825554d4cfb1515238de349da117
Jesper Håkansson <drager@programmers.se>
2015-06-01 21:17:06 +0200

    chore(coveralls): Use new version and increase retries

--- tool/travis.sh
+++ tool/travis.sh
@@ -10,10 +10,10 @@ dart --checked test/runner.dart
 # Install dart_coveralls
 # Rerun tests with coverage and send to coveralls
 if [ "$COVERALLS_TOKEN" ]; then
-  pub global activate dart_coveralls 0.1.11
+  pub global activate dart_coveralls
   pub global run dart_coveralls report \
     --token $COVERALLS_TOKEN \
-    --retry 2 \
+    --retry 10 \
     --throw-on-error \
     --debug \
     test/runner.dart
