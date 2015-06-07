7d15afa3f183621d532318c028cc81f1275d8dc6
Jesper Håkansson <drager@programmers.se>
2015-06-07 17:31:35 +0200

    doc: Added some examples

--- example/faker.dart
+++ example/faker.dart
@@ -3,4 +3,21 @@ library faker.example;
 import 'package:faker/faker.dart';
 
 main() {
+  // Generate some different types of data.
+  print(faker.address.country());
+  print(faker.company.name());
+  print(faker.currency.code());
+  print(faker.internet.ipv4Address());
+  print(faker.job.title());
+  print(faker.person.firstName());
+  print(faker.sport.name());
+
+  // Generate a random amount of IP v4 addresses (max 10).
+  print(random.amount((_) => faker.internet.ipv4Address(), 10));
+
+  // Generate random boolean.
+  print(random.boolean());
+
+  // Generate random decimal.
+  print(random.decimal());
 }
